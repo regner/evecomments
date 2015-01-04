058a6a4f2c75112f28c4249756ac9260e13654be
Regner Blok-Andersen <shadowdf@gmail.com>
2015-01-04 23:49:25 +0000

    Changes to support Cloud9 as a test

--- evecomments/settings.py
+++ evecomments/settings.py
@@ -1,12 +1,15 @@
 
 
+import os
+
+
 class Config(object):
     """ Default config values. """
     DEBUG = False
 
 
 class DevConfig(Config):
-    """ Production configuration. """
+    """ Development configuration. """
     DEBUG                   = True
     SECRET_KEY              = 'ThisIsJustTheDevKeyAndPrmesFromSomethingElseYBA*Sga78siTD&*SA%D&A*^STD&A^ISDAS'
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vagrant:vagrant@localhost/evecomments'
@@ -14,6 +17,18 @@ class DevConfig(Config):
     EVESSO                  = dict(
         consumer_key        ='56ef03e322fd422aab074c3cc90c3d01',
         consumer_secret     ='hfYxgV1EyYlH143bZcCBoXJw64v8u8AccAIBljQ4',
+        base_url            ='https://login.eveonline.com/oauth/',
+        access_token_url    ='https://login.eveonline.com/oauth/token',
+        access_token_method ='POST',
+        authorize_url       ='https://login.eveonline.com/oauth/authorize',
+    )
+
+
+class C9Config(DevConfig):
+    """ Development configuration for the C9 IDE """
+    EVESSO                  = dict(
+        consumer_key        ='7b4585e5f43842e7b19f007d671e6dd2',
+        consumer_secret     ='2NXRdItCcsixZAIN86LsqJfuAORsH269mnRfSEoT',
         base_url            ='https://login.eveonline.com/oauth/',
         access_token_url    ='https://login.eveonline.com/oauth/token',
         access_token_method ='POST',
--- run_app.py
+++ run_app.py
@@ -6,14 +6,20 @@ from flask.ext.script  import Manager, Shell, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from evecomments.app        import create_app
-from evecomments.settings   import DevConfig
+from evecomments.settings   import DevConfig, C9Config
 from evecomments.extensions import db
 
 if os.environ.get('EI_ENV') == 'prod':
     pass
     #app = create_app(ProdConfig)
+
+elif os.environ.get('EI_ENV') == 'c9':
+    app    = create_app(C9Config)
+    server = Server(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+
 else:
-    app = create_app(DevConfig)
+    app    = create_app(DevConfig)
+    server = Server()
 
 
 migrate = Migrate(app, db)
@@ -26,7 +32,7 @@ def _make_context():
     return {'app': app, 'db': db}
 
 
-manager.add_command('server',     Server())
+manager.add_command('server',     server)
 manager.add_command('shell',      Shell(make_context=_make_context))
 manager.add_command('db_migrate', MigrateCommand)
 
