27eef39c33c402db55d7f0289529eeb5e23014d0
Regner Blok-Andersen <shadowdf@gmail.com>
2015-01-12 12:08:44 +0000

    Adding some code comments to the comments api code and defaulting new comments to be posted by CCP FoxFour until auth has been sorted out.

--- evecomments/comments/api.py
+++ evecomments/comments/api.py
@@ -3,6 +3,7 @@
 from flask.ext.restful import Resource, reqparse, abort
 
 from evecomments.extensions      import db
+from evecomments.user.models     import UserModel
 from evecomments.sites.models    import SiteModel
 from evecomments.comments.models import CommentModel
 
@@ -30,7 +31,15 @@ class CommentV1(Resource):
     def get(self, site_id, comment_id):
         """ Returns a single comment. """
 
-        comment = CommentModel.query.filter_by(id=comment_id).first()
+        # Start by fetching the site specified
+        site = SiteModel.query.filter_by(id=site_id).first()
+
+        # We don't actually need the site object, we just want to make sure it exists
+        # We want to abort on the missing site rather than return 0 comments if the site doesn't exist
+        if site is None:
+            abort(404, message='Site {} does not exist.'.format(site_id))
+
+        comment = CommentModel.query.filter_by(site_id=site_id, id=comment_id).first()
         if comment is None:
             abort(404, message='Comment {} does not exist.'.format(comment_id))
 
@@ -50,11 +59,16 @@ class CommentCollectionV1(Resource):
     def get(self, site_id):
         """ Returns a list of all comments. """
 
+        # Start by fetching the site specified
         site = SiteModel.query.filter_by(id=site_id).first()
+
+        # We don't actually need the site object, we just want to make sure it exists
+        # We want to abort on the missing site rather than return 0 comments if the site doesn't exist
         if site is None:
             abort(404, message='Site {} does not exist.'.format(site_id))
 
-        comments = CommentModel.query.filter_by(site_id=site_id).all()
+        # Fetch and format the comments for the specified site
+        comments           = CommentModel.query.filter_by(site_id=site_id).all()
         formatted_comments = [format_comment_v1(comment) for comment in comments]
 
         return formatted_comments
@@ -62,14 +76,22 @@ class CommentCollectionV1(Resource):
     def post(self, site_id):
         """ Creates a new comment. """
 
+        # Start by fetching the site the comment is for
         site = SiteModel.query.filter_by(id=site_id).first()
+        
         if site is None:
             abort(404, message='Site {} does not exist.'.format(site_id))
 
+        # Parse the request data and confirm required fields are there
         args = self.reqparse.parse_args()
 
-        new_comment = CommentModel(site, args['message'])
+        # Until we have user auth working again assume CCP FoxFour (92168909)
+        user = UserModel.query.filter_by(id=92168909).first()
 
+        # Create a new comment
+        new_comment = CommentModel(site, args['message'], user)
+
+        # Deal with the parent comment if one is specified
         if 'parent' in args and args['parent'] is not None:
             parent = CommentModel.query.filter_by(id=args['parent']).first()
 
@@ -78,6 +100,7 @@ class CommentCollectionV1(Resource):
 
             new_comment.parent = parent
 
+        # Save the new comment to the db! :D
         db.session.add(new_comment)
         db.session.commit()
 
