d112dfed7f46892ddfa8c1da50c7b1269dccb7a2
Regner Blok-Andersen <shadowdf@gmail.com>
2015-01-23 02:11:37 +0000

    Updating the embed.js to complete the initial bootstrapping prototype

--- evecomments/static/js/embed.js
+++ evecomments/static/js/embed.js
@@ -1,5 +1,47 @@
 (function () {
     'use strict';
 
-    console.log(ec_site_id);
+    var ec_comments_uri = 'http://localhost:5000/api/v1/comments';
+
+    window.onload = addIframe;
+
+    function addIframe() {
+        var comments_div = document.getElementById('evecomments_thread');
+        var iframe       = document.createElement('iframe');
+
+        iframe.setAttribute('allowTransparency', 'true');
+        iframe.setAttribute('scrolling',         'no');
+        iframe.setAttribute('frameBorder',       '0');
+        iframe.setAttribute('src',               getCommentsUrl())
+
+        comments_div.appendChild(iframe);
+    };
+
+    function getCommentsUrl() {
+        var config    = getConfigObject();
+        var urlParams = encodeQueryData(config)
+
+        var commentsUrl = ec_comments_uri + '?' + urlParams
+
+        return commentsUrl
+    };
+
+    function getConfigObject() {
+        var config = {
+            'ec_site_id':      ec_site_id,
+            'ec_thread_id':    ec_thread_id,
+            'ec_thread_title': ec_thread_title,
+            'ec_thread_url':   ec_thread_url
+        }
+
+        return config
+    };
+
+    function encodeQueryData(data) {
+        var ret = [];
+
+        for (var d in data) ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+
+        return ret.join("&");
+    };
 }());
